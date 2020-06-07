2cc6c7e0471842367bc5444134a5f53f37c1316f
zaqwer101 <platonov256@gmail.com>
2020-06-07 11:30:26 +0800

    Получение списка ссылок на главы

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = 'https://readmanga.me/rassvet_iony'
-r = requests.get(URL)
+URL = 'https://readmanga.me'
+MANGA = 'rassvet_iony'
+r = requests.get(f'{URL}/{MANGA}')
 
 parser = BeautifulSoup(r.content, 'html.parser')
 
 data = parser.find_all('div')
 for elem in data:
     if 'class' in elem.attrs and 'chapters-link' in elem.attrs['class']:
-        chapter_links = elem
+        chapter_links_parser = elem
+        break
+
+if chapter_links_parser is None:
+    print("Can not find chapters list")
+    exit(1)
+
+chapter_links = []
+for elem in chapter_links_parser.find('table').find_all('a'):
+    chapter_links.append(URL + "/" + elem.attrs['href'])
+print(chapter_links)
