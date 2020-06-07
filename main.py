7001c1b694c9e97a19b1f30f9f1bacefb5e6fe59
zaqwer101 <platonov256@gmail.com>
2020-06-07 17:00:08 +0800

    Сборка томов в PDF

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+from PIL import Image
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import letter
 
 URL = 'https://readmanga.me'
 MANGA = 'rassvet_iony'
+
+
+def fillPage(path, canvas):
+    page_width, page_height = canvas._pagesize
+    image = Image.open(path)
+    draw_width, draw_height = page_width, page_height
+    canvas.setPageRotation(0)
+    canvas.drawImage(path, 0, 0, width=draw_width, height=draw_height, preserveAspectRatio=True)
+
+
+def createPDF(vol):
+    directory = f'{MANGA}/{vol}'
+    pdf = Canvas(f'{MANGA}/{MANGA}_{vol}.pdf')
+    for chapter in os.listdir(directory):
+        for image in os.listdir(directory +'/'+ chapter):
+            fillPage(directory +'/'+ chapter +'/'+ image, pdf)
+            pdf.showPage()
+    pdf.save()
+
 
 r = requests.get(f'{URL}/{MANGA}')
 
@@ -16,7 +38,7 @@ for elem in data:
         break
 
 if chapter_links_parser is None:
-    print("Can not find chapters list")
+    print("Не смог найти список глав")
     exit(1)
 ###
 
@@ -27,10 +49,16 @@ for elem in chapter_links_parser.find('table').find_all('a'):
 chapter_links.reverse()
 ###
 
+old_vol = chapter_links[0].split('/')[4] # чтобы понимать, когда начался новый том
 
 for chapter_link in chapter_links:
     # chapter_link - 'https://readmanga.me/rassvet_iony/vol1/1'
     vol = chapter_link.split('/')[4]
+
+    if vol != old_vol: # если это уже новый том - старый собираем в pdf
+        print("Собираем PDF")
+        createPDF(old_vol)
+
     chapter = chapter_link.split('/')[-1]
     current_directory = f'{MANGA}/{vol}/{chapter}'
 
@@ -51,3 +79,5 @@ for chapter_link in chapter_links:
         if not os.path.exists(filename):
             r = requests.get(image_link)
             open(filename, 'wb').write(r.content)
+    old_vol = vol
+createPDF(old_vol)
