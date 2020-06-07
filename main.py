d1a316677abe79d8b1bc160dcf2942a1ea5ca9d3
zaqwer101 <platonov256@gmail.com>
2020-06-07 12:08:54 +0800

    Тестовый прогон загрузки одной главы

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 URL = 'https://readmanga.me'
 MANGA = 'rassvet_iony'
+
 r = requests.get(f'{URL}/{MANGA}')
 
+## Парсим страницу манги на предмет глав
 parser = BeautifulSoup(r.content, 'html.parser')
-
 data = parser.find_all('div')
 for elem in data:
     if 'class' in elem.attrs and 'chapters-link' in elem.attrs['class']:
@@ -16,8 +18,26 @@ for elem in data:
 if chapter_links_parser is None:
     print("Can not find chapters list")
     exit(1)
+###
 
+## Преобразуем ссылки из chapter_links_parser в массив ссылок на главы
 chapter_links = []
 for elem in chapter_links_parser.find('table').find_all('a'):
     chapter_links.append(URL + "/" + elem.attrs['href'])
-print(chapter_links)
+chapter_links.reverse()
+###
+
+chapter_link = chapter_links[0]
+
+r = requests.get(chapter_link)
+content = r.content.decode('utf-8')
+chapter = content.split('rm_h.init( ')[1].split(', 0, false')[0]
+chapter = eval(chapter)
+
+for elem in chapter:
+    image_link = elem[0] + elem[2]
+    print(image_link)
+    filename = image_link.split('/')[-1]
+    file = open(filename, 'wb')
+    r = requests.get(image_link)
+    file.write(r.content)
